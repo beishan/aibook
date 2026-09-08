b8538c36e9d4933e3cf67c4e51547fe6b95c7f5e
weixueshan <weixueshan@staff.sinaft.com>
2026-09-08 20:54:01 +0800

    忽略 pnpm 本地存储目录

--- .pnpm-store/v11/projects/59c2178bb985e1605d7eca33a4dd5e96/src/utils/authSession.ts
+++
@@ -1,6 +0,0 @@
-export const AUTH_EXPIRED_EVENT = 'aibook:auth-expired'
-
-export const clearStoredAuthSession = () => {
-  localStorage.removeItem('token')
-  window.dispatchEvent(new Event(AUTH_EXPIRED_EVENT))
-}
